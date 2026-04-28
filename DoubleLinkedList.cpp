401b661df47a23d5a1f2be67d2e194240ed5a868
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2026-04-28 12:23:54 +0700

    insert node in ascending order

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -33,5 +33,29 @@ public:
         // step 2: assign value to the data field
         newNode->noMhs = nim;
 
+        // step 3: insert node in ascending order
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
+
+            // Step 4: make next of newnode = START
+            newNode->next = START;
+
+            // Step 5: START->prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
+
+            // Step 6: newNode->prev = NULL
+            newNode->prev = NULL;
+
+            // Step 7: START = newNode
+            START = newNode;
+            return;
+        }
+
     }
 };
